4044537894caa12127abdbc4bfa89e3066329bab
Yuri Pourre <yuripourre@gmail.com>
2017-08-11 15:50:31 -0300

    Add database error codes

---
+++ src/main/java/com/harium/database/DatabaseError.java
@@ -0,0 +1,7 @@
+package com.harium.database;
+
+public class DatabaseError {
+    private static final int ON_CREATE = -1;
+    private static final int ON_UPDATE = -2;
+    private static final int ON_DELETE = -3;
+}
